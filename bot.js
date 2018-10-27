bee87aef1c86c651c3d820f5096cee7e4ee92996
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-10-27 03:34:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,31 @@
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+console.log('By : Robben');
+console.log('ready');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: '•●{رَمِضّاِنُ كـــًرًيَـــمً ~☾',
+     details: للهم أختم بالصالحات أعمالنا,
+     url: 'http://twitch.tv/M7md_Salih',
+     state: في ڪڸ سن? هنآڪ شهر يجعڸني أإڪثر قرباً من ربي {شهر رمضآن},
+    application_id: '394600839496466443',
+     assets: {
+        small_image: 444859352093032449,
+        small_text: '2018',
+        large_image: 444864778465181706,
+        large_text: Ramadan Mubarak }
+
+  }
+    });
+});
+
+
+
+client.login(process.env.BOT_TOKEN); 
